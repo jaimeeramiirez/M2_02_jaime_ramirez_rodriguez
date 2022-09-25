c8dccde6250f76d6fc76fefe17d38a06dd8ac1cf
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-09-25 09:04:02 +0000

    EJERCICIO 3

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
+#EJERCICIO 1.- He creado 5 variables diferentes con valores ya establecidos. Luego he creado la variable solución que engloba la media aritmética de las 5 y la imprime en la pantalla.
 x1=8
 x2=7
 x3=9
 x4=9
 x5=6
-z =(((x1+x2+x3+x4+x5)/5))
-print("La nota media es", z)
+sol =(((x1+x2+x3+x4+x5)/5))
+print("La nota media es", sol)
 print("\n")
 
+#EJERCICIO 2.- La variable w es el resultado de esa operación. Para redondear el resultado utilizamos la función round, indicamos que queremos redondear y después de la coma indicamos a que decimal queremos redondearlo.
 w= (365/12)*14.7
 wround= round(w,2)
 print(wround)
+print("\n")
+
+#EJERCICIO 3.- Para que el usuario pueda introducir el texto que quiera utilizaremos la función input.
+
+username= input("Introduce your username: ")
+password= input("Introduce your password: ")
+
+if username == (3 <= len(username) < 10): 
+  print("True")
+else:
+  print("False")
+  
+if password == ("TOKIO" or password == "PYTHON"):
+  print("True")
+else: 
+  print("False");
+
+
+
+
+
 
