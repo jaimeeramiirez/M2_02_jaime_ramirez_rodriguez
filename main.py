738585b4e52b7a9df772c7c65fc84c38c8d78305
jaimeeramiirez <113980639+jaimeeramiirez@users.noreply.github.com>
2022-09-25 17:27:14 +0000

    all

--- main.py
+++ main.py
@@ -14,23 +14,35 @@ wround= round(w,2)
 print(wround)
 print("\n")
 
-#EJERCICIO 3.- Para que el usuario pueda introducir el texto que quiera utilizaremos la función input.
+
+#EJERCICIO 3.- Para que el usuario pueda introducir el texto que quiera utilizaremos la función input. Los condicionales los utilizamos para saber si una condición se cumple y sino lo hace denota False. En el primer condicional comprobamos que el nombre de usuario tenga una longitud de entre 3 y 10 caracteres; en el segundo comprobamos que la contraseña sea o "PYTHON" o "TOKIO".
 
 username= input("Introduce your username: ")
 password= input("Introduce your password: ")
 
 if (3 <= len(username) < 10): 
-  print("True")
+  print("\nTrue")
 else:
-  print("False")
+  print("\nFalse")
 
 if password == ("PYTHON") or password == ("TOKIO"):
-  print("True")
+  print("\nTrue")
 else: 
-  print("False")
+  print("\nFalse\n\n")
 
 
 
+#EJERCICIO 4
 
+num1=5
+num2=13
 
+num1 +=1
+print(num1)
+
+num2 +=2
+print(num2)
+
+num1= num1*3
+print(num1)
 
